9cc0f903f935d402a56b962e7572b5a4f56cafbc
FourSqweez <four7518@gmail.com>
2021-04-02 13:37:39 +0700

    createed todoSlice

---
+++ src/components/Input.js
@@ -0,0 +1,31 @@
+import './Input.css'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { saveTodo } from '../features/todoSlice'
+
+export default function Input() {
+  const [input, setInput] = useState('')
+  const dispatch = useDispatch()
+
+  const addTodo = () => {
+    console.log(`Adding ${input}`)
+
+    dispatch(
+      saveTodo({
+        item: input,
+        done: false,
+        id: Date.now(),
+      })
+    )
+  }
+  return (
+    <div className="input">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={addTodo}>Add!</button>
+    </div>
+  )
+}
